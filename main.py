dbc01561c4961f1b6a98e700d375d0c8c0120ee9
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2024-06-29 20:52:41 +0200

    Move module importing to separate component

--- main.py
+++ main.py
@@ -6,65 +6,20 @@
 
 # External Imports
 import gc
-import os.path
 
 # Internal Imports
-import modules
 from components.check_updates import CheckUpdates
 from components.check_updates import VERSION_CURRENT
 from components.restore_files import RestoreFiles
 from components.logger        import LoggerInit
 from components.logger        import SEPARATOR
 from components.xml_write     import ExportXML
-
-
-# Retrieve a list of file types based on the modules.
-# '__modules__' is a dict of all modules' names and objects
-# Refer to 'modules/__init__.py' for relevant code. 
-FILE_TYPES_LIST = [] 
-for extension in modules.__modules__.keys():
-  extension = '*.' + extension
-  file_type_part = ('Model', extension)
-  FILE_TYPES_LIST.append(file_type_part)
-
-
-# Function that outputs module data
-def ModuleData():
-  
-  print('List of installed modules:\n')
-  
-  for module_name in modules.__modules__:
-    
-    module = modules.__modules__[module_name]
-    data = module.module_data
-    
-    print(SEPARATOR[0:29])
-    for key in data:
-      print(f'{key} : {data[key]}')
-
-
-
-# Output the appropriate model data Extract function
-def ProcessModules(file_name):
-
-  file_extension = file_name.split('.')[-1]
-
-  # "__modules__" is a dict of all modules' names and objects
-  # Refer to "modules/__init__.py" for relevant code
-  if file_extension in modules.__modules__.keys():
-
-    extract_module = modules.__modules__[file_extension]
-    return extract_module.Extract, extract_module.module_data
-
-  else:
-
-    raise Exception('Unrecognized file type!')
+from components.module_import import ProcessModules
+from components.module_import import ModuleData
+from components.module_import import FILE_TYPES_LIST
 
 
 def Main(file_names, template, no_export_file, strip_armature_tree):
-
-  if not modules.__modules__:
-    raise Exception(f'No modules found.')
 
   # Initiate Logger
   logger_filename = 'kozmadeus.log'
