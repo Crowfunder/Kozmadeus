d94e80a0a4450e099b706213b5190bf9abcad262
Crowfunder <52300171+Crowfunder@users.noreply.github.com>
2024-09-15 13:30:13 +0200

    Bump version to v1.0.0

--- utils/check_updates.py
+++ utils/check_updates.py
@@ -11,7 +11,7 @@ from wget import download as DownloadFile
 
 
 # Constants
-VERSION_CURRENT = 'v0.1.0beta'
+VERSION_CURRENT = 'v1.0.0'
 VERSION_FILENAME = 'VERSION'
 VERSION_URL = 'https://raw.githubusercontent.com/Crowfunder/Kozmadeus/main/VERSION'
 
@@ -20,27 +20,27 @@ VERSION_URL = 'https://raw.githubusercontent.com/Crowfunder/Kozmadeus/main/VERSI
 # version as string, the rest of the logic is to be handled by UI
 def CheckUpdates():
 
-  # Special file handle for disabling update check
-  if os.path.isfile('UPDATER_DISABLE'):
+    # Special file handle for disabling update check
+    if os.path.isfile('UPDATER_DISABLE'):
+        output_version = dict()
+        output_version['current'] = VERSION_CURRENT
+        output_version['fetched'] = VERSION_CURRENT
+        return output_version
+
+    # Delete leftover VERSION file if there is any
+    if os.path.isfile(VERSION_FILENAME):
+        DeleteFile(VERSION_FILENAME)
+
+    DownloadFile(VERSION_URL, bar=None, out=VERSION_FILENAME)
+    with open(VERSION_FILENAME, 'r') as version_file:
+        version_github = version_file.read()
+    
+    # Strip string out of newlines etc
+    version_github = re.sub(r"[\n\t\s]*", "", version_github)
+  
+    # Form output dict
     output_version = dict()
     output_version['current'] = VERSION_CURRENT
-    output_version['fetched'] = VERSION_CURRENT
+    output_version['fetched'] = version_github
+
     return output_version
-
-  # Delete leftover VERSION file if there is any
-  if os.path.isfile(VERSION_FILENAME):
-    DeleteFile(VERSION_FILENAME)
-
-  DownloadFile(VERSION_URL, bar=None, out=VERSION_FILENAME)
-  with open(VERSION_FILENAME, 'r') as version_file:
-    version_github = version_file.read()
-    
-  # Strip string out of newlines etc
-  version_github = re.sub(r"[\n\t\s]*", "", version_github)
-  
-  # Form output dict
-  output_version = dict()
-  output_version['current'] = VERSION_CURRENT
-  output_version['fetched'] = version_github
-
-  return output_version
